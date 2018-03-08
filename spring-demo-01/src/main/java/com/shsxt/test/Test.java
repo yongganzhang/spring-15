9210506edc613f43b6cff686ad9a7950246c98dd
yongganzhang <1285963544@qq.com>
2018-03-08 13:24:43 +0800

    12

--- spring-demo-01/src/main/java/com/shsxt/test/Test.java
+++ spring-demo-01/src/main/java/com/shsxt/test/Test.java
@@ -6,16 +6,22 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.shsxt.demo.HelloService;
 
 public class Test {
-	
+
 	public static void main(String[] args) {
-		
+
 		// 加载好配置文件夹
 		ApplicationContext act = new ClassPathXmlApplicationContext("spring-context.xml");
-		
-		HelloService hs =  (HelloService)act.getBean("helloService");
-		
-		hs.print();
-		
+
+		HelloService hs = (HelloService) act.getBean("helloService");
+
+		System.out.println(hs);
+		for (int i = 0; i < 10; i++) {
+			hs = (HelloService) act.getBean("helloService");
+			System.out.println(hs);
+		}
+		hs = (HelloService) act.getBean("helloService");
+		System.out.println(hs);
+
 	}
 
 }
