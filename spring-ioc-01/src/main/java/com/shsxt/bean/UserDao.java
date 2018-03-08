88b38e3adcb57a7395911f2446a2130bb20166b6
yongganzhang <1285963544@qq.com>
2018-03-08 16:53:42 +0800

    提交DI

---
+++ spring-ioc-01/src/main/java/com/shsxt/bean/UserDao.java
@@ -0,0 +1,5 @@
+package com.shsxt.bean;
+
+public class UserDao {
+
+}
--- spring-ioc-01/src/main/java/com/shsxt/bean/UserSerivce.java
+++ spring-ioc-01/src/main/java/com/shsxt/bean/UserSerivce.java
@@ -2,6 +2,8 @@ package com.shsxt.bean;
 
 public class UserSerivce {
 	
+	private UserDao dao;// 属性输入
+	
 	public void print () {
 		System.out.println("hello static factory !");
 	}
